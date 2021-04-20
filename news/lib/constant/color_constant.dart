ee828c291c141ef77e7eb5ece6a7e4cc63c141ab
Melisa Akyol <46058934+melisakyoll@users.noreply.github.com>
2021-04-20 22:45:51 +0300

    constant folder added.

---
+++ news/lib/constant/color_constant.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+class ColorConstants {
+  static ColorConstants instance = ColorConstants._init();
+
+  ColorConstants._init();
+
+  final darkBurgundy = Color(0xFF7B0707);
+  final japaneseLaurel = Color(0xFF247B07);
+  final downy = Color(0xFF67BFD0);
+  final goldenGrass = Color(0xFFDD9536);
+  final yellowGreen = Color(0xFFB0D56D);
+  final sage = Color(0xFFA0AB8C);
+  final starship = Color(0xFFE7E93F);
+  final easternBlue = Color(0xFF1BA5B1);
+  final neptune = Color(0xFF6EAFB4);
+}
