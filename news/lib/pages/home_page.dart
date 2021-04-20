9450c6e84b23014d25d7c48d09bde3806f8012f9
Melisa Akyol <46058934+melisakyoll@users.noreply.github.com>
2021-04-20 23:50:37 +0300

    home_page

--- news/lib/pages/home_page.dart
+++ news/lib/pages/home_page.dart
@@ -4,15 +4,14 @@ import 'package:news/models/article.dart';
 import 'package:news/screen/drawer.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class Anasayfa extends StatefulWidget {
-  Anasayfa({
-    Key key,
-  }) : super(key: key);
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
   @override
-  _AnasayfaState createState() => _AnasayfaState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _AnasayfaState extends State<Anasayfa> {
+class _HomePageState extends State<HomePage> {
   List<Articles> articles = [];
 
   @override
@@ -29,53 +28,74 @@ class _AnasayfaState extends State<Anasayfa> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Color(0xff7b0707),
-        title: Text(
-          "YENİ YAŞAM",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-      ),
+      appBar: buildAppBar(),
       body: Center(
         child: ListView.builder(
           itemCount: articles.length,
           itemBuilder: (context, index) {
-            return Card(
-              child: Column(
-                children: [
-                  Image.network(articles[index].urlToImage),
-                  ListTile(
-                    title: Text(articles[index].title,
-                        style: TextStyle(fontSize: 19.0)),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 15.0, top: 15.0),
-                    child: Text(articles[index].description),
-                  ),
-                  ButtonBar(
-                    alignment: MainAxisAlignment.start,
-                    children: [
-                      // ignore: deprecated_member_use
-                      FlatButton(
-                        onPressed: () {
-                          launch(articles[index].url);
-                        },
-                        child: Text("Habere Git"),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            );
+            return  buildNews(index);
           },
         ),
       ),
       drawer: DrawerMenu(),
     );
   }
+
+  Card buildNews(int index) {
+    return Card(
+          child: Column(
+            children: [
+              Image.network(articles[index].urlToImage),
+              ListTile(
+                title: Text(articles[index].title,
+                    style: TextStyle(fontSize: 19.0)),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 15.0, top: 15.0),
+                child: Text(articles[index].description),
+              ),
+              ButtonBar(
+                alignment: MainAxisAlignment.start,
+                children: [
+                  // ignore: deprecated_member_use
+                  FlatButton(
+                    onPressed: () {
+                      launch(articles[index].url);
+                    },
+                    child: Text("Habere Git"),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+  }
 }
+
+AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: Color(0xff7b0707),
+      title: Text(
+        "YENİ YAŞAM",
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      centerTitle: true,
+    );
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
