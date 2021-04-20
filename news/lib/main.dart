bccd67fad6ace4ad0ba989471a7d3f96ccd3f731
Melisa Akyol <46058934+melisakyoll@users.noreply.github.com>
2021-04-20 23:51:11 +0300

    main.dart

--- news/lib/main.dart
+++ news/lib/main.dart
@@ -1,23 +1,36 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:news/login/sign_in.dart';
+import 'package:news/login/sign_up.dart';
 import 'package:news/screen/SplashScreen.dart';
-import 'auth/girisYap.dart';
-import 'pages/anasayfa.dart';
-import 'pages/ayarlar.dart';
-import 'auth/kayit.dart';
+import 'pages/home_page.dart';
+import 'pages/settings.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp( MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: "/",
-    routes: {
-      "/": (BuildContext context) => SplashScreen(),
-      "/Anasayfa": (BuildContext context) => Anasayfa(),
-      "/Giris": (BuildContext context) => GirisYap(),
-      "/Kayit": (BuildContext context) => KayitOl(),
-      "/Ayarlar": (BuildContext context) => Ayarlar(),
-    },
-  ));
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp (
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primaryColor: const Color(0xFFEF6C00),
+          accentColor: Color(0xFFEF6C00),
+          iconTheme: IconThemeData(color: Colors.white)),
+      initialRoute: "/",
+      routes: {
+        "/": (BuildContext context) => SplashScreen(),
+        "/Anasayfa": (BuildContext context) => HomePage(),
+        "/Giris": (BuildContext context) => GirisYap(),
+        "/Kayit": (BuildContext context) => KayitOl(),
+        "/Ayarlar": (BuildContext context) => Ayarlar(),
+      },
+      home: SplashScreen(),
+    );
+  }
+}
+
